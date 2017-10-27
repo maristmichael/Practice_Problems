92ed73422ca75a42daa2684231d761116b8a204e
Michael Gutierrez <michael.gutierrez2@marist.edu>
2017-10-26 22:53:47 -0400

    Updates explanation to 100 doors problem

--- 100_Doors.py
+++ 100_Doors.py
@@ -1,12 +1,12 @@
-# 100 Doors question
+# 100 Doors Problem
 #
 # There are 100 doors numbered 1-100 and are all closed
 # You make 100 passes by the doors starting with the first door
-# The first pass you visit every door and switch the doors'state (if closed you open and vice versa)
-# The second pass you visit any door whose number is divisible by 2
-# The third pass you visit any door whose number is divisible by 3
-# You keep going until you only pass the 100th door and then stop
-# Which doors are open?
+# The first pass, you visit every door and switch the doors' state (if the door is closed you open it and vice versa)
+# The second pass, you visit and switch the state of any door whose number is divisible by 2
+# The third pass, you visit and switch the state of any door whose number is divisible by 3
+# You keep going until you only visit and switch the 100th door
+# Which doors are open after 100 passes?
 
 door_list = []
 doors_open = []
